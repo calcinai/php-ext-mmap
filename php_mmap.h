50772409f2e197d4a6990eab2218c595319c9198
Martin Madsen <github@martinpetermadsen.com>
2017-11-19 16:15:24 +0000

    write and seek API fix
    The arguments on stream write and seek functions did not match API spec
    (at least not on PHP7 compiled for ARM).

--- php_mmap.h
+++ php_mmap.h
@@ -8,7 +8,7 @@ PHP_FUNCTION(mmap_open);
 extern zend_module_entry mmap_module_entry;
 #define phpext_mmap_ptr &mmap_module_entry
 
-size_t mmap_stream_write(php_stream *stream, char *buffer, size_t length TSRMLS_DC);size_t mmap_stream_read(php_stream *stream, char *buffer, size_t length TSRMLS_DC);int mmap_stream_flush(php_stream *stream TSRMLS_DC);int mmap_stream_seek(php_stream *stream, off_t offset, int whence, off_t *newoffset TSRMLS_DC);
+size_t mmap_stream_write(php_stream *stream, const char *buffer, size_t length TSRMLS_DC);size_t mmap_stream_read(php_stream *stream, char *buffer, size_t length TSRMLS_DC);int mmap_stream_flush(php_stream *stream TSRMLS_DC);int mmap_stream_seek(php_stream *stream, zend_off_t offset, int whence, zend_off_t *newoffset);
 int mmap_stream_close(php_stream *stream, int close_handle TSRMLS_DC);
 
 struct mmap_stream_data { 
